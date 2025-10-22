fdbd0bb0d4aa072ac15ce168da74b600dd447319
onaki88 <onaki88@gmail.com>
2025-10-22 22:42:15 +0100

    fixed rooms.ts

--- src/services/rooms.ts
+++ src/services/rooms.ts
@@ -1,55 +1,38 @@
 import { Room, Media } from "@/types";
 
 export function mapRawToRooms(raw: any): Room[] {
-  let variants: any[] = [];
-  raw.rooms_by_serial_no[0].rooms.forEach((r: any) => {
-    variants.push(
-      r.variants.reduce((prev: any, curr: any) => {
-        const variant = {
-          ...curr,
-          hotel_room_type_code: r.room_type_code,
-          hotel_properties: r.properties,
-          hotel_name: r.name,
-          hotel_location: raw.hotel_details.address
-            ? `${raw.hotel_details.address.city}, ${raw.hotel_details.address.country}`
-            : "",
-        };
-        return {
-          ...prev,
-          ...variant,
-        };
-      })
-    );
-  });
-
   let rooms: Room[] = [];
-  variants.forEach((r: any) => {
-    let media: Media = {};
-    if (r.hotel_properties.video_url) {
-      Object.assign(media, { video_url: r.hotel_properties.video_url.med });
-    }
 
-    if (r.hotel_properties.room_images) {
-      Object.assign(media, {
-        room_images: r.hotel_properties.room_images[0].image_urls,
-      });
-    }
+  raw.rooms_by_serial_no[0].rooms.forEach((r: any) => {
+    r.variants.forEach((v: any) => {
+      let media: Media = {};
+      if (r.properties.video_url) {
+        Object.assign(media, { video_url: r.properties.video_url.med });
+      }
 
-    let room: Room = {
-      id: r.variant_id,
-      name: r.hotel_name, //`${r.hotel_name} - ${r.name}`,
-      amenities: r.display_properties.map((p: any) => p.value),
-      rating: 4, // to do
-      location: `${raw.hotel_details.address.city}, ${raw.hotel_details.address.country}`,
-      price: r.total_price.discounted_price || r.total_price.total_price,
-      currency: r.total_price.currency,
-      discount: r.total_price.promo_list[0].offer_title || "",
-      priceWithoutDiscount: r.total_price.total_price,
-      priceInfo: r.price_info,
-      media: media,
-    };
-    rooms.push(room);
+      if (r.properties.room_images) {
+        Object.assign(media, {
+          room_images: r.properties.room_images[0].image_urls,
+        });
+      }
+
+      let room: Room = {
+        id: v.variant_id,
+        name: `${r.name} - ${v.name}`,
+        amenities: v.display_properties.map((p: any) => p.value),
+        rating: 4, // to do
+        location: `${raw.hotel_details.address.city}, ${raw.hotel_details.address.country}`,
+        price: v.total_price.discounted_price || v.total_price.total_price,
+        currency: v.total_price.currency,
+        discount: v.total_price.promo_list[0].offer_title || "",
+        priceWithoutDiscount: v.total_price.total_price,
+        priceInfo: v.price_info,
+        media: media,
+      };
+      rooms.push(room);
+    });
   });
+
   return rooms;
 }
 
